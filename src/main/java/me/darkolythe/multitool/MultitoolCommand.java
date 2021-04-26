b64ea872f53e67c125361878718bcb6bd1bfdc58
christopherwalkerml <christopherwalkerml@gmail.com>
2021-04-26 17:47:27 -0400

    feature - 3.4.6 - cleaned up command code a bit

--- src/main/java/me/darkolythe/multitool/MultitoolCommand.java
+++ src/main/java/me/darkolythe/multitool/MultitoolCommand.java
@@ -20,103 +20,103 @@ public class MultitoolCommand implements CommandExecutor {
 				sender.sendMessage(main.messages.get("msgwrongworld"));
 				return true;
 			}
-			if (player.hasPermission("multitool.command")) {
-				if (cmd.getName().equalsIgnoreCase("Multitool")) {
-					if (args.length == 1) {
-						if (args[0].equalsIgnoreCase("Open") || args[0].equalsIgnoreCase("O")) {
-							if (player.hasPermission("multitool.use")) {
-								player.openInventory(main.multitoolutils.getToolInv(player));
-							} else {
-								sender.sendMessage(main.messages.get("msgnopermission"));
-							}
-						} else if (args[0].equalsIgnoreCase("Toggle") || args[0].equalsIgnoreCase("T")) {
-							main.multitoolutils.setToggle(player.getUniqueId(), !main.multitoolutils.getToggle(player.getUniqueId()));
-							if (main.multitoolutils.getToggle(player.getUniqueId())) {
-								sender.sendMessage(main.messages.get("msgtoggleon"));
-							} else {
-								sender.sendMessage(main.messages.get("msgtoggleoff"));
-							}
-						} else if (args[0].equalsIgnoreCase("Reload")) {
-							if (player.hasPermission("multitool.reload")) {
-								main.multitoolutils.getConfigs();
-								player.sendMessage(main.prefix + ChatColor.GREEN + "Files have been reloaded");
-							} else {
-								sender.sendMessage(main.messages.get("msgnopermission"));
-							}
-						} else if (args[0].equalsIgnoreCase("Wings") || args[0].equalsIgnoreCase("W")) {
-							if (player.hasPermission("multitool.multiarmour")) {
-								player.openInventory(main.multitoolutils.getWingInv(player));
-							} else {
-								sender.sendMessage(main.messages.get("msgnopermission"));
-							}
-						} else if (args[0].equalsIgnoreCase("migrate")) {
-							if (player.hasPermission("multitool.migrate")) {
-								sender.sendMessage(main.messages.get("msginvalid") + " /mt migrate [old, database]");
-							} else {
-								sender.sendMessage(main.messages.get("msgnopermission"));
-							}
-						} else {
-							if (!player.hasPermission("multitool.migrate")) {
-								sender.sendMessage(main.messages.get("msginvalid") + " /mt [open, wings, toggle]");
-							} else {
-								sender.sendMessage(main.messages.get("msginvalid") + " /mt [open, wings, toggle, migrate, reload]");
+			if (!player.hasPermission("multitool.command")) {
+				sender.sendMessage(main.messages.get("msgnopermission"));
+				return true;
+			}
+
+			if (args.length == 1) {
+				if (args[0].equalsIgnoreCase("Open") || args[0].equalsIgnoreCase("O")) {
+					if (player.hasPermission("multitool.use")) {
+						player.openInventory(main.multitoolutils.getToolInv(player));
+					} else {
+						sender.sendMessage(main.messages.get("msgnopermission"));
+					}
+				} else if (args[0].equalsIgnoreCase("Toggle") || args[0].equalsIgnoreCase("T")) {
+					main.multitoolutils.setToggle(player.getUniqueId(), !main.multitoolutils.getToggle(player.getUniqueId()));
+					if (main.multitoolutils.getToggle(player.getUniqueId())) {
+						sender.sendMessage(main.messages.get("msgtoggleon"));
+					} else {
+						sender.sendMessage(main.messages.get("msgtoggleoff"));
+					}
+				} else if (args[0].equalsIgnoreCase("Reload")) {
+					if (player.hasPermission("multitool.reload")) {
+						main.multitoolutils.getConfigs();
+						player.sendMessage(main.prefix + ChatColor.GREEN + "Files have been reloaded");
+					} else {
+						sender.sendMessage(main.messages.get("msgnopermission"));
+					}
+				} else if (args[0].equalsIgnoreCase("Wings") || args[0].equalsIgnoreCase("W")) {
+					if (player.hasPermission("multitool.multiarmour")) {
+						player.openInventory(main.multitoolutils.getWingInv(player));
+					} else {
+						sender.sendMessage(main.messages.get("msgnopermission"));
+					}
+				} else if (args[0].equalsIgnoreCase("migrate")) {
+					if (player.hasPermission("multitool.migrate")) {
+						sender.sendMessage(main.messages.get("msginvalid") + " /mt migrate [old, database]");
+					} else {
+						sender.sendMessage(main.messages.get("msgnopermission"));
+					}
+				} else {
+					if (!player.hasPermission("multitool.migrate")) {
+						sender.sendMessage(main.messages.get("msginvalid") + " /mt [open, wings, toggle]");
+					} else {
+						sender.sendMessage(main.messages.get("msginvalid") + " /mt [open, wings, toggle, migrate, reload]");
+					}
+				}
+			} else if (args.length == 2) {
+				if (args[0].equalsIgnoreCase("Open") || args[0].equalsIgnoreCase("O")) {
+					if (player.hasPermission("multitool.viewothers")) {
+						for (Player players : Bukkit.getServer().getOnlinePlayers()) {
+							if (args[1].equalsIgnoreCase(players.getName())) {
+								player.openInventory(main.multitoolutils.getToolInv(players));
+								main.openinv.put(player, players);
+								return true;
 							}
 						}
-					} else if (args.length == 2) {
-						if (args[0].equalsIgnoreCase("Open") || args[0].equalsIgnoreCase("O")) {
-							if (player.hasPermission("multitool.viewothers")) {
-								for (Player players : Bukkit.getServer().getOnlinePlayers()) {
-									if (args[1].equalsIgnoreCase(players.getName())) {
-										player.openInventory(main.multitoolutils.getToolInv(players));
-										main.openinv.put(player, players);
-										return true;
-									}
-								}
-								sender.sendMessage(main.messages.get("msgnotonline"));
+						sender.sendMessage(main.messages.get("msgnotonline"));
+					} else {
+						sender.sendMessage(main.messages.get("msgnopermission"));
+					}
+				} else if (args[0].equalsIgnoreCase("Migrate")) {
+					if (player.hasPermission("multitool.migrate")) {
+						if (args[1].equalsIgnoreCase("old")) {
+							player.sendMessage(main.prefix + ChatColor.WHITE + "The server may lag depending on the amount of saved multitools...");
+							if (main.multitoolutils.migrate()) {
+								player.sendMessage(main.prefix + ChatColor.WHITE + "Migration done. Multitools were successfully transferred");
 							} else {
-								sender.sendMessage(main.messages.get("msgnopermission"));
+								player.sendMessage(main.prefix + ChatColor.WHITE + "No multitools from the regular version of the plugin were found. Migration done.");
 							}
-						} else if (args[0].equalsIgnoreCase("Migrate")) {
-							if (player.hasPermission("multitool.migrate")) {
-								if (args[1].equalsIgnoreCase("old")) {
-									player.sendMessage(main.prefix + ChatColor.WHITE + "The server may lag depending on the amount of saved multitools...");
-									if (main.multitoolutils.migrate()) {
-										player.sendMessage(main.prefix + ChatColor.WHITE + "Migration done. Multitools were successfully transferred");
-									} else {
-										player.sendMessage(main.prefix + ChatColor.WHITE + "No multitools from the regular version of the plugin were found. Migration done.");
-									}
-								} else if (args[1].equalsIgnoreCase("database")) {
-									main.multitoolutils.transferToDatabase(player);
-								} else {
-									sender.sendMessage(main.messages.get("msginvalid") + " /mt migrate [old, database]");
-								}
-							} else {
-								sender.sendMessage(main.messages.get("msgnopermission"));
-							}
-						} else if (args[0].equals("Wings") || args[0].equalsIgnoreCase("W")) {
-							if (player.hasPermission("multitool.viewothers")) {
-								for (Player players : Bukkit.getServer().getOnlinePlayers()) {
-									if (args[1].equalsIgnoreCase(players.getName())) {
-										player.openInventory(main.multitoolutils.getWingInv(players));
-										main.openinv.put(player, players);
-										return true;
-									}
-								}
-								sender.sendMessage(main.messages.get("msgnotonline"));
-							} else {
-								sender.sendMessage(main.messages.get("msgnopermission"));
-							}
+						} else if (args[1].equalsIgnoreCase("database")) {
+							player.sendMessage(main.prefix + ChatColor.WHITE + "The server may lag depending on the amount of saved multitools...");
+							main.multitoolutils.transferToDatabase(player);
+						} else {
+							sender.sendMessage(main.messages.get("msginvalid") + " /mt migrate [old, database]");
 						}
 					} else {
-						if (!player.hasPermission("multitool.migrate")) {
-							sender.sendMessage(main.messages.get("msginvalid") + " /mt [open, wings, toggle]");
-						} else {
-							sender.sendMessage(main.messages.get("msginvalid") + " /mt [open, wings, toggle, migrate, reload]");
+						sender.sendMessage(main.messages.get("msgnopermission"));
+					}
+				} else if (args[0].equals("Wings") || args[0].equalsIgnoreCase("W")) {
+					if (player.hasPermission("multitool.viewothers")) {
+						for (Player players : Bukkit.getServer().getOnlinePlayers()) {
+							if (args[1].equalsIgnoreCase(players.getName())) {
+								player.openInventory(main.multitoolutils.getWingInv(players));
+								main.openinv.put(player, players);
+								return true;
+							}
 						}
+						sender.sendMessage(main.messages.get("msgnotonline"));
+					} else {
+						sender.sendMessage(main.messages.get("msgnopermission"));
 					}
 				}
 			} else {
-				sender.sendMessage(main.messages.get("msgnopermission"));
+				if (!player.hasPermission("multitool.migrate")) {
+					sender.sendMessage(main.messages.get("msginvalid") + " /mt [open, wings, toggle]");
+				} else {
+					sender.sendMessage(main.messages.get("msginvalid") + " /mt [open, wings, toggle, migrate, reload]");
+				}
 			}
 		}
 		return true;
